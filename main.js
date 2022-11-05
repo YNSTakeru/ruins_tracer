51f728c4c9491d0b5f63d54b5cc08fbb2957bf6c
YNS <95395710+YNSTakeru@users.noreply.github.com>
2022-11-06 03:18:26 +0900

    テスト

--- main.js
+++ main.js
@@ -68,6 +68,8 @@ function success(pos) {
 
     if (!_data) return;
 
+    _myPosition.heading = 1;
+
     _ruinNames.forEach((name) => {
         r = geod.Inverse(
             _myPosition.latitude,
@@ -102,34 +104,6 @@ function error(err) {
     _data = data;
     _ruinNames = ruinNames;
     _circles = createDOM(ruinNames);
-
-    if (_myPosition) {
-        let geod = geodesic.Geodesic.WGS84,
-            r;
-
-        let theta;
-
-        _ruinNames.forEach((name) => {
-            r = geod.Inverse(
-                _myPosition.latitude,
-                _myPosition.longitude,
-                _data[name].latitude,
-                _data[name].longitude
-            );
-            const distance = r.s12.toFixed(3);
-            const r2 = (distance * (42.5 - 1.5)) / 5000;
-
-            if (distance <= 5000) {
-                if (_myPosition.heading)
-                    theta =
-                        ((90 - _myPosition.heading - r.azi1) * Math.PI) / 180;
-                _circles[name].style.transform = `translate(calc(-50% + ${
-                    r2 * Math.cos(theta)
-                }vw), calc(-50% - ${r2 * Math.sin(theta)}vw))`;
-                _circles[name].style.visibility = "visible";
-            }
-        });
-    }
 })();
 
 function disableScroll(event) {
@@ -137,3 +111,125 @@ function disableScroll(event) {
 }
 
 document.addEventListener("touchmove", disableScroll, { passive: false });
+
+let os;
+
+// DOM構築完了イベントハンドラ登録
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    // 簡易的なOS判定
+    os = detectOSSimply();
+    if (os == "iphone") {
+        // safari用。DeviceOrientation APIの使用をユーザに許可して貰う
+        document
+            .querySelector("#permit")
+            .addEventListener("click", permitDeviceOrientationForSafari);
+
+        window.addEventListener("deviceorientation", orientation, true);
+    } else if (os == "android") {
+        window.addEventListener("deviceorientationabsolute", orientation, true);
+    } else {
+        window.alert("PC未対応サンプル");
+    }
+}
+
+// ジャイロスコープと地磁気をセンサーから取得
+function orientation(event) {
+    let absolute = event.absolute;
+    let alpha = event.alpha;
+    let beta = event.beta;
+    let gamma = event.gamma;
+
+    let degrees;
+    if (os == "iphone") {
+        // webkitCompasssHeading値を採用
+        degrees = event.webkitCompassHeading;
+        const $compass = document.querySelector("#compass");
+        $compass.textContent = degrees;
+    } else {
+        // deviceorientationabsoluteイベントのalphaを補正
+        degrees = compassHeading(alpha, beta, gamma);
+    }
+
+    let direction;
+    if ((degrees > 337.5 && degrees < 360) || (degrees > 0 && degrees < 22.5)) {
+        direction = "北";
+    } else if (degrees > 22.5 && degrees < 67.5) {
+        direction = "北東";
+    } else if (degrees > 67.5 && degrees < 112.5) {
+        direction = "東";
+    } else if (degrees > 112.5 && degrees < 157.5) {
+        direction = "東南";
+    } else if (degrees > 157.5 && degrees < 202.5) {
+        direction = "南";
+    } else if (degrees > 202.5 && degrees < 247.5) {
+        direction = "南西";
+    } else if (degrees > 247.5 && degrees < 292.5) {
+        direction = "西";
+    } else if (degrees > 292.5 && degrees < 337.5) {
+        direction = "北西";
+    }
+}
+
+// 端末の傾き補正（Android用）
+// https://www.w3.org/TR/orientation-event/
+function compassHeading(alpha, beta, gamma) {
+    var degtorad = Math.PI / 180; // Degree-to-Radian conversion
+
+    var _x = beta ? beta * degtorad : 0; // beta value
+    var _y = gamma ? gamma * degtorad : 0; // gamma value
+    var _z = alpha ? alpha * degtorad : 0; // alpha value
+
+    var cX = Math.cos(_x);
+    var cY = Math.cos(_y);
+    var cZ = Math.cos(_z);
+    var sX = Math.sin(_x);
+    var sY = Math.sin(_y);
+    var sZ = Math.sin(_z);
+
+    // Calculate Vx and Vy components
+    var Vx = -cZ * sY - sZ * sX * cY;
+    var Vy = -sZ * sY + cZ * sX * cY;
+
+    // Calculate compass heading
+    var compassHeading = Math.atan(Vx / Vy);
+
+    // Convert compass heading to use whole unit circle
+    if (Vy < 0) {
+        compassHeading += Math.PI;
+    } else if (Vx < 0) {
+        compassHeading += 2 * Math.PI;
+    }
+
+    return compassHeading * (180 / Math.PI); // Compass Heading (in degrees)
+}
+
+// 簡易OS判定
+function detectOSSimply() {
+    let ret;
+    if (
+        navigator.userAgent.indexOf("iPhone") > 0 ||
+        navigator.userAgent.indexOf("iPad") > 0 ||
+        navigator.userAgent.indexOf("iPod") > 0
+    ) {
+        // iPad OS13のsafariはデフォルト「Macintosh」なので別途要対応
+        ret = "iphone";
+    } else if (navigator.userAgent.indexOf("Android") > 0) {
+        ret = "android";
+    } else {
+        ret = "pc";
+    }
+
+    return ret;
+}
+
+function permitDeviceOrientationForSafari() {
+    DeviceOrientationEvent.requestPermission()
+        .then((response) => {
+            if (response === "granted") {
+                window.addEventListener("deviceorientation", detectDirection);
+            }
+        })
+        .catch(console.error);
+}
