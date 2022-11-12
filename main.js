a817725df2861d2ac1ec37fb5ad8b70f895461ba
YNS <95395710+YNSTakeru@users.noreply.github.com>
2022-11-12 22:15:15 +0900

    テスト

--- main.js
+++ main.js
@@ -189,7 +189,6 @@ function setupCamera() {
     // ).textContent = `${screen.width} x ${screen.height}`;
 
     video.style.zIndex = 10000;
-    video.style.width = "100%";
 
     const promise = navigator.mediaDevices.getUserMedia(medias);
 
